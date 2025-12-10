bbcce94c5e3e85e1400815c1d813d1797158e429
anol123 <anol.roy123@gmail.com>
2025-12-10 23:21:01 +0530

    Added binary search code

--- src/search.rs
+++ src/search.rs
@@ -1,9 +1,23 @@
-struct Record{
+struct Record {
     id: u32,
     name: String,
 }
 
-fn binary_serach(records: &[Record], target_id:u32){
-    let mut low =0;
+fn binary_serach(records: &[Record], target_id: u32)-> Option<&Record> {
+    let mut low = 0;
     let mut high = records.len();
+
+    while low < high {
+        let mid = (low + high) / 2;
+        let mid_id = records[mid].id;
+
+        if mid_id ==target_id{
+            return Some(&records[mid]);
+        } else if mid_id< target_id{
+            low=mid+1;
+        } else{
+            high= mid-1;
+        }
+    }
+    None
 }
