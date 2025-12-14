91f01eccac0bb5a12a8f954536ed4a9d88b8a05f
anol123 <anol.roy123@gmail.com>
2025-12-14 23:30:09 +0530

    wip

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,21 @@ fn main() {
             id: 2,
             name: "Alice".to_string(),
         },
-        Record { id: 3, name: "Bob".to_string() },
-        Record { id: 5, name: "Charlie".to_string() },
-        Record { id: 7, name: "David".to_string() },
-        Record { id: 10, name: "Eve".to_string() },
+        Record {
+            id: 3,
+            name: "Bob".to_string(),
+        },
+        Record {
+            id: 5,
+            name: "Charlie".to_string(),
+        },
+        Record {
+            id: 7,
+            name: "David".to_string(),
+        },
+        Record {
+            id: 10,
+            name: "Eve".to_string(),
+        },
     ];
 }
