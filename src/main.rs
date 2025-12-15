11a89e0265e63ec5c342709406bbd71436d9299b
anol123 <anol.roy123@gmail.com>
2025-12-15 22:20:27 +0530

    Added the code to handle quit case

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::{self, Write};
+
 use crate::search::Record;
 
 mod search;
@@ -29,4 +31,25 @@ fn main() {
             name: "Eve".to_string(),
         },
     ];
+
+    //take user input of id they want to search
+    //take user input of wheather search or quit
+
+    loop {
+        println!("Enter an id to search (or 'q' to quit):");
+        io::stdout().flush().unwrap();
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Error reading input");
+
+        let mut trimmed = input.trim();
+
+        //handle quit case
+        if trimmed.eq_ignore_ascii_case("q") {
+            println!("Goodbye!");
+            break;
+        }
+    }
 }
