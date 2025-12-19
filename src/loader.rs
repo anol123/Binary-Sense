53a6143a4d50a7c08c6bea819198fb327f41b35d
anol123 <anol.roy123@gmail.com>
2025-12-19 23:44:10 +0530

    wip

--- src/loader.rs
+++ src/loader.rs
@@ -1,9 +1,18 @@
 use std::{error::Error, fs::File};
 
+use csv::ReaderBuilder;
+
 use crate::search::Record;
 
-pub fn load_records_from_csv(path: &str)->Result<Vec<Record>, Box<dyn Error>>{
+pub fn load_records_from_csv(path: &str) -> Result<Vec<Record>, Box<dyn Error>> {
     let file = File::open(path)?;
-    todo!()
+    let mut reader = ReaderBuilder::new().trim(csv::Trim::All).from_reader(file);
+    let mut records = Vec::new();
 
+    for result in reader.deserialize() {
+        let record: Record = result?;
+        records.push(record);
+    }
+
+    Ok(records)
 }
